0663008af842e668e03b0f4ae08a77f5d9673a0e
Colin Graf <cgraf@informatik.uni-bremen.de>
2011-06-24 00:10:38 +0200

    Use a public domain implementation of getopt (for windows)

--- src/Tools/Win32/getopt.h
+++ src/Tools/Win32/getopt.h
@@ -5,9 +5,26 @@
 extern "C" {
 #endif
 
-extern char *optarg; /* Global argument pointer. */
-extern int optind; /* Global argv index. */
-extern int getopt(int argc, char *argv[], const char *optstring);
+extern char *optarg; /* pointer to current option argument */
+extern int optind; /* index for first non-option arg     */
+extern int opterr; /* enable built-in error messages     */
+extern int optopt; /* return value for option being evaluated   */
+
+struct option
+{
+  const char* name;
+  int has_arg;
+  int* flag;
+  int val;
+};
+
+#define	no_argument		0
+#define required_argument	1
+#define optional_argument	2
+
+int getopt(int argc, char* const argv[], const char* optstring);
+int getopt_long(int argc, char* const argv[], const char* optstring, const struct option* opts, int* index);
+int getopt_long_only(int argc, char* const argv[], const char* optstring, const struct option* opts, int* index);
 
 #ifdef __cplusplus
 };
