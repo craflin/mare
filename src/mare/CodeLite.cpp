b4bd8f2f1d61898c42b8e9620318b00133ec33b1
Colin Graf <cgraf@informatik.uni-bremen.de>
2011-09-27 17:09:28 +0200

    codelite: Use "textMode" for custom build commands

--- src/mare/CodeLite.cpp
+++ src/mare/CodeLite.cpp
@@ -129,9 +129,9 @@ bool CodeLite::readFile()
         }
         */
 
-        engine.getKeys("buildCommand", projectConfig.buildCommand, false);
-        engine.getKeys("reBuildCommand", projectConfig.reBuildCommand, false);
-        engine.getKeys("cleanCommand", projectConfig.cleanCommand, false);
+        engine.getText("buildCommand", projectConfig.buildCommand, false);
+        engine.getText("reBuildCommand", projectConfig.reBuildCommand, false);
+        engine.getText("cleanCommand", projectConfig.cleanCommand, false);
         projectConfig.buildDir = engine.getFirstKey("buildDir", true);
 
         engine.getText("command", projectConfig.command, false);
