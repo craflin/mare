9ca8cdfb9effdedf23eeed6fb1c8c71d74b706e1
Colin Graf <cgraf@informatik.uni-bremen.de>
2011-12-01 12:52:26 +0100

    codeblocks: Use addCommandLineKey for command line keys

--- src/mare/CodeBlocks.cpp
+++ src/mare/CodeBlocks.cpp
@@ -57,7 +57,7 @@ bool CodeBlocks::generate(const Map<String, String>& userArgs)
 
   // add user arguments
   for(const Map<String, String>::Node* i = userArgs.getFirst(); i; i = i->getNext())
-    engine.addDefaultKey(i->key, i->data);
+    engine.addCommandLineKey(i->key, i->data);
 
   //
   if(!readFile())
