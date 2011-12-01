6eb784e1089104c5b9dbafb82f4093988d1666ab
Colin Graf <cgraf@informatik.uni-bremen.de>
2011-12-01 12:52:15 +0100

    cmake: Use addCommandLineKey for command line keys

--- src/mare/CMake.cpp
+++ src/mare/CMake.cpp
@@ -62,7 +62,7 @@ bool CMake::generate(const Map<String, String>& userArgs)
 
   // add user arguments
   for(const Map<String, String>::Node* i = userArgs.getFirst(); i; i = i->getNext())
-    engine.addDefaultKey(i->key, i->data);
+    engine.addCommandLineKey(i->key, i->data);
 
   // step #1: read input file
   if(!readFile())
