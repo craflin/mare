80e5742e4de27a3a8bd3ee74fe8eae9d4340fa7a
Colin Graf <colin.graf@sovereign-labs.com>
2014-02-18 21:40:45 +0100

    Remove compilation warning

--- src/mare/Tools/md5.cpp
+++ src/mare/Tools/md5.cpp
@@ -146,7 +146,7 @@ void MD5::final(unsigned char digest[16])
     transform(ctx->buf, (uint32 *) ctx->in);
     byteReverse((unsigned char *) ctx->buf, 4);
     memcpy(digest, ctx->buf, 16);
-    memset(ctx, 0, sizeof(ctx));        /* In case it's sensitive */
+    memset(ctx, 0, sizeof(MD5));        /* In case it's sensitive */
 }
 
 
